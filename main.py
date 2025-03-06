331070bdfc9a428e9484de50680834142d7e566c
Idrus ramadhhan <idrusramadhan24@gmail.com>
2025-03-07 06:21:35 +0700

    add character count functionality and update main logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import numbers_of_words
+from stats import get_count_words
 def get_book_text(path):
     with open(path) as file:
         file_contents = file.read()
@@ -8,8 +9,9 @@ def main():
     filepath = "books/frankenstein.txt"
     get_text = get_book_text(filepath)
     count_words = numbers_of_words(get_text)
+    get_chars = get_count_words(get_text)
     print(f"{count_words} words found in the document")
-
+    print(get_chars)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,18 @@
+
+
 def numbers_of_words(text):
     split = text.split()
     return len(split)
+
+
+def get_count_words(text):
+    dict = {}
+    for char in text:
+        lowered_char = char.lower()
+        if lowered_char in dict:
+            dict[lowered_char] += 1
+        else:
+            dict[lowered_char] = 1
+    return dict
+
+
